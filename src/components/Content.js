63d70e0c3ac5bd1f8f5b4904766c829bfee41be1
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-10-06 21:49:20 -0500

    adding files to my repository

---
+++ src/components/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+
+    constructor(props){
+        this.state = [];
+        
+    }
+
+
+}
