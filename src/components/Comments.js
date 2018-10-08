03ebd8a6abfcafbe9578f403e7f1d3724a2f5e36
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-10-08 02:12:46 -0500

    starting on my adding comments section

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -3,19 +3,41 @@ import PropTypes from 'prop-types';
 import './css/content.scss';
 
 class Comments extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            comments: props.dataProp.comments,
+            comment: ""
+        }
+    }
+   
+    
+    addNewComment(event) {
+        const textValue = event.target.value;
+        const theComments = this.state.comments.slice();
+        console.log(theComments);
+        
+        // theComments = theComments.push({
+        //     text: textValue,
+        //     username: [event.target.name],
+        // })
+        if(event.keyCode === 13){
+        }
+    }
     
     render() {
         const props = this.props;
-        return(
+        return(           
             <div className="comments">
                 <p className="likes">{props.dataProp.likes} likes</p>
-                {props.dataProp.comments.map( (item,index) => {
+                {this.state.comments.map( (item,index) => {
                     return(
                         <div className="comments-section" key={index}>
                         <p><span>{item.username}</span>{item.text}</p>
                         </div>
                     )
                 })}
+                <input type="text" name={props.dataProp.username} onChange={this.addNewComment} />
             </div>
         )
     }
