419f682359d12fad724072f8621ff3c06dac6c6f
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-10-10 14:28:36 -0500

    added my bootstrap dependencies

--- src/Loading.js
+++ src/Loading.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import styled ,{ css } from 'styled-components';
+import {
+    Button,
+    Input
+} from 'reactstrap';
 
 class Loading extends Component {
     render(){
         return(
            <form onSubmit={this.props.submit}>
-              <input type="text" placeholder="username" name="username" onChange={this.props.usernameInput}/>
-              <input type="text" placeholder="password" name="password" onChange={this.props.passwordInput}/>
-              <input type="submit" value="submit" onSubmit={this.props.submit}/>
+              <Input type="text" placeholder="username" name="username" onChange={this.props.usernameInput}/>
+              <Input type="text" placeholder="password" name="password" onChange={this.props.passwordInput}/>
+              <Button type="submit" value="submit" onSubmit={this.props.submit}>Log In</Button>
            </form>
         )
     } 
